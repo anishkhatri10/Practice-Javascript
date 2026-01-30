1bf346747971a028cf87db49823a6ad4ed4a7bc9
anish khatri <gk087277@gmail.com>
2026-01-30 08:38:11 +0545

    first commit for js

---
+++ first.js
@@ -0,0 +1,11 @@
+alert("welcome to my new sesion"); // and windows.alert
+
+// variable in javascript topic
+// important topic= Null and undefine (understanding this topic ) 
+fulName = "anish";
+age = 19;
+price = 99.99;
+x = null;
+isFollow = true
+console.log(fulName);
+
